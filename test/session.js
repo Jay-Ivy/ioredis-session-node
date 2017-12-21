6c63fe93a1d9f60344481d974bcf2f9f47a68f3d
jie.ding <jie.ding@msxf.com>
2017-12-21 11:05:27 +0800

    [MOD]readme

--- test/session.js
+++ test/session.js
@@ -4,8 +4,8 @@ global.Redis = require('../lib/session');
 
 Redis({
     sessionKey: 'testsid',            // session名称
-    sidPrefix: 'test.msxf.com',                  // session id value 前缀
-    secret: 'sessionsecret.msxf.com',      // 加密盐
-    expires: 30 * 60 * 1000,     // 过期时间（毫秒）
-    redisNodes: []               // 集群节点配置
+    sidPrefix: 'www.jay.com',         // session id value 前缀
+    secret: Date.now(),               // 加密盐
+    expires: 30 * 60 * 1000,          // 过期时间（毫秒）
+    redisNodes: []                    // 集群节点配置
 });
